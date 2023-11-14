e4d1ae60c49e95d26d14c94338606e5450fcc09f
AlaaEldin <alaa12122000@gmail.com>
2023-11-14 15:19:40 +0300

    Added parse.c

---
+++ parse.c
@@ -0,0 +1,96 @@
+#include "main.h"
+
+/**
+ * parse - to parse the input line
+ * @line: the line parsed
+ * Return: array of splitted command
+*/
+
+char **parse(char *line)
+{
+        int len = 0, cap = ARGBUF;
+        char *token, **tokens;
+
+        tokens = malloc(cap * sizeof(char *));
+        if (!tokens)
+        {
+                perror("./hsh: ");
+                return (NULL);
+        }
+        token = strtok(line, DELIMIT);
+        while (token)
+        {
+                tokens[len++] = token;
+                if (len >= cap)
+                {
+                        cap += (ARGBUF + 1);
+                        tokens = realloc(tokens, cap * sizeof(char *));
+                        if (!tokens)
+                        {
+                                perror("./hsh: ");
+                                return (NULL);
+                        }
+                }
+                tokens = strtok(NULL, DELIMIT);
+        }
+        tokens[len] = NULL;
+        parse_var(tokens);
+        return (tokens);
+}
+
+/**
+ * parse_var - replaces variables if there
+ * @args: the input tokens
+ * Return: 0 if success, otherwise 1
+*/
+
+int parse_var(char **args)
+{
+        int i, j, k = 1, l;
+        pid_t pid = getpid();
+        char *env_var;
+
+        for (i = 0; args[i]; i++)
+        {
+                for (j = 0; args[i][j]; j++)
+                {
+                        if (args[i][j] = '$' && args[i][j + 1] == '$')
+                        {
+                                sprintf(args[i] + j, "%d", pid);
+                                k = 0;
+                        }
+                        else if (args[i][j] = '$' && args[i][j + 1] == '?')
+                        {
+                                sprintf(args[i] + j, "%d", exit_stat);
+                                k = 0;
+                        }
+                        else if (args[i][j] == '#') /*Handle comments*/
+                        {
+                                args[i][0] = '\0';
+                                for (l = i + 1; args[l]; l++)
+                                        args[l] = NULL;
+                                return (0);
+                        }
+                        else if (args[i][0] == '$' && args[i][1] != '$'
+                                                && args[i][1] != '?')
+                        {
+                                env_var = _getenv(args[i]);
+                                if (env_var && args[i][0] == '$')
+                                {
+                                        strcpy(args[i], env_var);
+                                        free(env_var);
+                                }
+                                k = 0;
+                        }
+                        else if (args[i][j] == '\\')
+                        {
+                                char ch;
+
+                                ch = args[i][j + 1];
+                                replaceOrRemove(args[i], ch);
+                        }
+                }
+        }
+
+        return (k);
+}
